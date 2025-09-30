8f141b5dd1259173305911f659c2a917b9a0fa9f
noejunior792 <noedombaxe@gmail.com>
2025-09-30 11:30:53 +0100

    fix: Add --check-origin=false to ttyd to fix proxy issue

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Inicia o ttyd na porta fornecida pelo Render (a variável $PORT).
-# Isso apresentará um prompt de login do sistema no terminal web.
-ttyd --port ${PORT:-10000} login
+# A opção --check-origin=false é adicionada para tentar resolver problemas de conexão por trás de proxies.
+ttyd --port ${PORT:-10000} --check-origin=false login
